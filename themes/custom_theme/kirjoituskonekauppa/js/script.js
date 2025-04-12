cd56b65c6de5a3722f52bd2878ccbbe20fa6cd9c
ellinor <ellinoora.riskala@metropolia.fi>
2025-04-12 19:19:16 +0300

    tää paska on nyt valmis

---
+++ themes/custom_theme/kirjoituskonekauppa/js/script.js
@@ -0,0 +1,7 @@
+(function ($, Drupal) {
+  Drupal.behaviors.kirjoituskonekauppaBehavior = {
+    attach: function (context, settings) {
+      console.log('kirjoituskonekauppa theme is active');
+    }
+  };
+})(jQuery, Drupal);
